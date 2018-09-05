3f0cfad68756c6a4f35cdf701ed7d2bc167c976e
panthazin.oo <PanThazin.Oo@nttdata.com>
2018-09-05 15:27:28 +0630

    sentence added

--- OnlineTest/src/Example/AddedNewjavaClass2InBranch1.java
+++ OnlineTest/src/Example/AddedNewjavaClass2InBranch1.java
@@ -5,6 +5,8 @@ public class AddedNewjavaClass2InBranch1 {
 	public static void main(String[] args) {
 		System.out.println("New String Added!!");
 		System.out.println("New Java Class Added");
+		System.out.println("Sentence1 Added");
+		System.out.println("Sentence2 Added");
 	}
 
 }
