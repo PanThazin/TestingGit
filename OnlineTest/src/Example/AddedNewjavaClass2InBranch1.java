ae61b532eee1be6436b5d67a06f4cb37c48c3104
panthazin <PanThazin.Oo@nttdata.com>
2018-09-05 10:57:26 +0630

    add new string

--- OnlineTest/src/Example/AddedNewjavaClass2InBranch1.java
+++ OnlineTest/src/Example/AddedNewjavaClass2InBranch1.java
@@ -3,6 +3,7 @@ package Example;
 public class AddedNewjavaClass2InBranch1 {
 
 	public static void main(String[] args) {
+		System.out.println("New String Added!!");
 		System.out.println("New Java Class Added");
 	}
 
