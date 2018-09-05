ff0603b0985aa3347df59c5f62897e0fc647c6a2
panthazin.oo <PanThazin.Oo@nttdata.com>
2018-09-05 08:39:14 +0630

    RebaseTest.java

---
+++ OnlineTest/src/RebaseTest.java
@@ -0,0 +1,23 @@
+
+public class RebaseTest {
+
+	public static void main(String[] args) {
+		byte b;
+		int i = 258;
+		double d = 325.59;
+
+		b = (byte) i;
+		System.out.println(b);
+
+		i = (int) d;
+		System.out.println(i);
+
+		b = (byte) d;
+		System.out.println(b);
+		
+		double STATIC = 2.5 ;
+		System.out.println( STATIC );
+
+	}
+
+}
